d7797605d4f2f971e5d2cf692b5c7f4180d43242
MindulaHewadewage <mindufernando@gmail.com>
2023-01-23 15:42:44 +0100

    fixed

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+export default {
+  data() {
+    return {
+      title: ''
+    }
+  },
+  components: {
+    AppTitle,
+  }
+}
+</script>
+
+
+<template>
+  <AppTitle />
+</template>
+
+<style>
+
+</style>
